26798f903aa23525a0a92f6d7906ae91a80bc9b5
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-09-18 10:41:24 +0530

    s

--- Ex_on_lists.py
+++ Ex_on_lists.py
@@ -19,20 +19,37 @@ list2 = ["Dear", "Sir"]
 i=0
 j=0
 list3 = []
-while(i< len(list1)):
-  list3.append(list1[i] + list2[j])
-  i+= 1
+while(j< len(list1)):
+  while(i< len(list2)):
+    list3.append(list1[j] + list2[i])
+    i+= 1
+  j += 1
+  i=0
 print(list3)
 
 #Question 4: Remove empty strings from the list of strings
 list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
-i=0
+i=list1.count("")
+while i>=1:
+  list1.remove("")
+  i -= 1
 print(list1)
+
 # Question 5: Given a Python list, find value 20 in the list, and if it is present, replace it with 200. Only update the first occurrence of a value
 list1 = [5, 10, 15, 20, 25, 50, 20]
 list1.insert(list1.index(20), 200)
 list1.pop(list1.index(20))
 print(list1)
+
 # Question 6: Given a Python list, remove all occurrence of 20 from the list
 list1 = [5, 20, 15, 20, 25, 50, 20]
 i=list1.count(20)
+while i>=1:
+  list1.remove(20)
+  i -= 1
+print(list1)
+
+#Question 7: Given a Python list, remove 1st occurrence of 20 from the list
+list1 = [5, 20, 15, 20, 25, 50, 20]
+list1.remove(20)
+print(list1)
---
+++ Ex_on_sets.py
@@ -0,0 +1,50 @@
+#Exercise Question 1: Add a list of elements to a given set
+sampleSet = {"Yellow", "Orange", "Black"}
+sampleList = ["Blue", "Green", "Red"]
+sampleSet.update(sampleList)
+print(sampleSet)
+
+#Exercise Question 2: Return a set of identical items from a given two Python set
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(set1.intersection(set2))
+
+#Exercise Question 3: Returns a new set with all items from both sets by removing duplicates
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(set1.union(set2))
+
+#Exercise Question 4: Given two Python sets, update first set with items that exist only in the first set and not in the second set.
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+set1 = set1.difference(set2)
+print(set1)
+
+#Exercise Question 5: Remove 10, 20, 30 elements from a following set at once
+set1 = {10, 20, 30, 40, 50}
+set1.remove(10)
+set1.remove(20)
+set1.remove(30)
+print(set1)
+
+#Exercise Question 6: Return a set of all elements in either A or B, but not both
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(set1.symmetric_difference(set2))
+
+#Exercise Question 7: Determines whether or not the following two sets have any elements in common. If yes display the common elements
+set1 = {10, 20, 30, 40, 50}
+set2 = {60, 70, 80, 90, 10}
+print(set1.intersection(set2))
+
+#Exercise Question 8: Update set1 by adding items from set2, except common items
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(set2.symmetric_difference(set1))
+
+#Exercise Question 9: Remove items from set1 that are not common to both set1 and set2
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+set1.remove
+set1.update(set1.intersection(set2))
+print(set1)
