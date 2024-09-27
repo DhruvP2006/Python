c59b64d6d6c182ad9722df9ff02bf812fb651e26
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-09-27 10:55:21 +0530

    s

---
+++ Assignment-2.py
@@ -0,0 +1,8 @@
+num = input()
+
+for i in num:
+  if(i%2 == 0):
+    print(i)
+for i in num:
+  if(i%2 != 0):
+    print(i)
---
+++ Decision Control Statements.py
@@ -0,0 +1,81 @@
+# a = input()
+# b = input()
+# if(a == b):
+#  print(a, " is equal to ", b)
+# elif  (a<b):
+#   print(a, " is less than ", b)
+# elif(a>b):
+#   print(a, " is greater than ", b)
+# j = 0
+# for i in range(2, 20, 2):
+#   j = j + i
+# print(j)
+# j = 0
+# for i in range(1, 11):
+#   j = i*i
+#   print(j)
+# j = 0
+# l1 = [ 24, 20, 18, 23, 21, 16]
+# for i in l1:
+#   j = j + i
+# j = j/len(l1)
+# print(j)
+
+# for i in range(1, 21):
+#   if(i%2==0):
+#     print(i)
+# for i in range(1, 21):
+#   if(i%2!=0):
+#     print(i)
+
+# for i in range(9, 51):
+#   if(i%2 == 0):
+#     print(i)
+
+# for i in range(1, 5):
+#   for j in range(1, 5):
+#     print(i*j)
+#     if(j>4):
+#       break
+
+# for i in 'Dhruv Pankhania':
+#   if i == 'a':
+#     continue
+#   print(i)
+
+# sum = 0
+# for i in range(2, 22, 2):
+#   sum = sum+ i
+# print(sum)
+
+# num = [1, 2, 3, 4, 5, 6]
+# for i in num:
+#   print(i*i)
+
+# for i in range(51):
+#   if(i%2 == 0):
+#     print(i)
+  
+# for i in range(5):
+#   for j in range(1, i+1):
+#     print("*", end=" ")
+#   print(' ')
+
+# b = 0
+# for  i in range(5, 0, -1):
+#   b += 1
+#   for j in range(0, i):
+#     print(b, end=" ")
+#   print("\r")
+
+# count = 1
+# while(count<5):
+#   print(count)
+#   count += 1
+
+# i = 99
+# j = 0
+# while(i>10):
+#   i = i/3
+#   j += 1
+# print(j)
