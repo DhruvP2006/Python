8f07b1567bd88a2fa448bbfe3ff0193992814731
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-10-07 15:52:15 +0530

    1

---
+++ EXP4.2_C4-1_06.py
@@ -0,0 +1,13 @@
+#Write a Python program for a character frequency counter function that takes a list of strings from the user as input and displays the frequency of each character in the list.
+def letter_frequency(a):
+  a = str(a).upper()
+  b = list(a)
+  c = list(set(a))
+  d = {}
+  i = 0
+  while(i < len(c)):
+    d.update({c[i]:b.count(c[i])})
+    i += 1
+  return d
+e = letter_frequency(input("Enter a String:"))
+print(e)
