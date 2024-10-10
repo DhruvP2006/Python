9663989554af11baf5a2cc5fd97a4939c4cd1e9e
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-10-10 23:53:04 +0530

    recursive functions

--- EXP4.1_C4-1_06.py
+++ EXP4.1_C4-1_06.py
@@ -1,10 +1,14 @@
 #Write a Python program using a recursive function that takes a string as input from the user and displays whether the string is Palindrome.
-def Palindrome(j):
-      if(j == j[::-1]):
-        print("The given string is a Palindrome")
-      else:
-        print("The given string is Not a Palindrome")
-a = input("Do you want to Check Palindrome? (Y/N): ")
-while(a == "Y"):
-  Palindrome(input("Enter a String to check if it`s a Plaindrome or not:"))
-  a = input("Do you want to Check Palindrome again? (Y/N): ")
+string = input("Enter a String:");
+def check_palindrome(a):
+  if(len(a)<=1):
+    return True
+  if(a[0]==a[-1]):
+    return True
+  else:
+    return False
+  check_palindrome(a[1:-1])
+if(check_palindrome(string)):
+  print("The given string is a Palindrome")
+else:
+  print("The given string is not a Palindrome")
