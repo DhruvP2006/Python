3e8f5ff573f0ccb79a3bc5117ec8596cc87080a6
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-11-22 10:27:02 +0530

    matplotlib

--- Ex_on_matplotlib.py
+++ Ex_on_matplotlib.py
@@ -1,6 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-plt.plot(xpoints, ypoints)
+# xpoints = np.array([1, 2, 6, 8])
+# ypoints = np.array([3, 8 ,1, 10])
+# plt.plot(ypoints, color="r", marker="o", mfc="b", mec="#010101", lw="20")
+# plt.show()
+
+
+# x1 = np.array([0, 1, 2, 3])
+# y1 = np.array([3, 8, 1, 10])
+# x2 = np.array([0, 1, 2, 3])
+# y2 = np.array([6, 2, 7, 11])
+# plt.plot(x1, y1)
+# plt.plot(x2, y2)
+# plt.show()
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+font1 = {'family':'sans-serif', 'color':'blue', 'size':'20'}
+font2 = {'family':'sans-serif', 'color':'red', 'size':'20'}
+
+plt.xlabel("Average Pulse", fontdict=font1)
+plt.ylabel("Calorie Burnage")
+plt.title("Average Pulse v/s Calorie Burnage Graph")
+plt.plot(x, y)
 plt.show()
+
--- Module_3.py
+++ Module_3.py
@@ -29,7 +29,38 @@
 
 
 # Write a function login(username, password) that checks if both the username and password meet certain conditions (e.g., username must be alphanumeric, password must be at least 8 characters). Display appropriate error messages if the inputs don't meet the
-username = input("Enter the Username")
-password = input("Enter the Password")
-def login(x, y):
-  if(x.isalphanum() and y.isalphanum()):
+# username = input("Enter the Username: ")
+# password = input("Enter the Password: ")
+# def login(x, y):
+#   if(x.isalnum()):
+#     print("username must be alphanumeric")
+#   if(y.isalnum()):
+#     print("Password must be alphanumeric")
+#   if(len(y)<8):
+#     print("Password must be at least 8 characters")
+# login(username, password)
+
+# Write a program that asks the user for a range of numbers and then counts how many of those numbers are even and how many are odd.
+# a = int(input("Enter a range of Number: "))
+# odd_count = 0
+# even_count = 0
+# for i in range(a):
+#   if(i%2 == 0):
+#     even_count += 1
+#   else:
+#     odd_count += 1
+
+# print(f"Number of even numes is {even_count}")    
+# print(f"Number of odd numes is {odd_count}")
+
+# Write a Python program that takes a list of dictionaries, where each dictionary represents a student with their name and scores in various subjects. The program should return a new dictionary where each key is a student's name, and the value is their average score across all subjects. (Using Function)
+
+students = [ {'name': 'Arjun', 'math': 85, 'science': 90, 'english': 78}, {'name': 'Balram',
+'math': 92, 'science': 88, 'english': 84}, {'name': 'Damodar', 'math': 72, 'science': 75,
+'english': 80} ]
+
+# Write a program to generate calender of a month given the start_day and the number of days in that month.
+
+start_day = (input("Enter the Start Day: "))
+Number_of_Days = int(input("Enter the Number of days"))
+print("S M T W T F S")
